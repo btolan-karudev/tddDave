730413a811137ab7aa3fd5fe627c20b75f3960b0
Beniamin Tolan <beniamin.tolan@outlook.com>
2020-11-05 12:34:36 +0100

    tdd autoload

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -39,8 +39,8 @@ class ComposerStaticInit3e6ba0f7bd5a7859cf54e1e6b691a234
         'phpDocumentor\\Reflection\\' => 
         array (
             0 => __DIR__ . '/..' . '/phpdocumentor/reflection-common/src',
-            1 => __DIR__ . '/..' . '/phpdocumentor/type-resolver/src',
-            2 => __DIR__ . '/..' . '/phpdocumentor/reflection-docblock/src',
+            1 => __DIR__ . '/..' . '/phpdocumentor/reflection-docblock/src',
+            2 => __DIR__ . '/..' . '/phpdocumentor/type-resolver/src',
         ),
         'Webmozart\\Assert\\' => 
         array (
